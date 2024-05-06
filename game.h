ab29dcbe029c7812be5b5f6fd257d841ee5b2af6
cbgiveup <168111022+cbgiveup@users.noreply.github.com>
2024-05-06 10:09:29 -0400

    Update game.h

--- game.h
+++ game.h
@@ -9,12 +9,8 @@ private:
     bool cleared;
 public:
     Game();
-    Game(string, string);
     string getName();
     string getStory();
     bool getCleared();
-    void setName(string);
-    void setStory(string);
     void setCleared(bool);
-    void customize(bool);
 };
