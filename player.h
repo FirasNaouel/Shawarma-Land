ebf4d1c2ba4953f5feeba485d199dc3fd2762097
cbgiveup <168111022+cbgiveup@users.noreply.github.com>
2024-05-06 10:11:00 -0400

    Create player.h

---
+++ player.h
@@ -0,0 +1,12 @@
+#include "coa.h"
+
+using namespace std;
+
+class Player : public COA {
+    private:
+    static const int emojiSize = 5;
+    string emojis[emojiSize] = {"\U0001F600", "\U0001F606", "\U0001F602", "\U0001F911", "\U0001F607"};
+    public:
+    Player();
+    void customize(bool);
+};
