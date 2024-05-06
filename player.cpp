cdf26a6b79af00b539920d99691963166ab6be4a
cbgiveup <168111022+cbgiveup@users.noreply.github.com>
2024-05-06 10:11:14 -0400

    Create player.cpp

---
+++ player.cpp
@@ -0,0 +1,24 @@
+#include "player.h"
+
+using namespace std;
+
+Player::Player() : COA() {
+    name = "Zara";
+    emoji = "\U0001F600";
+    health = 1;
+    speed = 2;
+    strength = 3;
+}
+
+void Player::customize(bool c) {
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    if (c) {
+        cout << "What would you like to call your character?" << endl;
+        getline(cin, name);
+        cout << "What would you like to set as your emoji? (Enter digit corresponding to emoji)" << endl;
+        for (int i = 0; i < emojiSize; i++) {
+            cout << i << ": " << emojis[i] << endl;
+        }
+        emoji = emojis[Program::question(0, 4)];
+    }
+}
