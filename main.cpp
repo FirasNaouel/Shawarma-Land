7471506616d1998d1024614f5464f7214e747cee
cbgiveup <168111022+cbgiveup@users.noreply.github.com>
2024-05-06 10:09:13 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,30 @@
-#include "program.h"
+#include "player.h"
+
 using namespace std;
 
-void intro(Game& game, Program& backend) {
-    system("clear");
+void intro(Game& game) {
+    Program::clearScreen();
     cout << game.getStory() << endl;
-    cout << "Welcome to " << backend.boldON() << game.getName() << backend.boldOFF() << endl;
+    cout << endl << "Welcome to " << Program::bold() << game.getName() << Program::boldOFF() << endl;
+}
+
+void intro(Player& player) {
+    cout << "Character: " << Program::bold() << player.getName() << Program::boldOFF() << endl;
+    cout << "Emoji: " << player.getEmoji() << endl;
+    cout << "Health: " << player.getHealth() << endl;
+    cout << "Speed: " << player.getSpeed() << endl;
+    cout << "Strength: " << player.getStrength() << endl;
 }
 
 int main() {
-    Program backend;
     Game game;
-    game.customize(backend.yesNo("Would you like to customize the game? (y/n)", '-', 'y', 'n'));
-    intro(game, backend);
+    Player player;
+    while(!game.getCleared()) {
+        intro(game);
+        Program::enterContinue();
+        Program::clearScreen();
+        player.customize(Program::question("Would you like to customize your character? (y/n)", 'y', 'n'));
+        intro(player);
+        game.setCleared(true);
+    }
 }
