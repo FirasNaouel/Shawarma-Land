90f975c2b4bb65dffff183828daa8bb1e1d16090
cbgiveup <168111022+cbgiveup@users.noreply.github.com>
2024-05-06 10:09:45 -0400

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 Game::Game() {
-    name = "Player";
-    story = "";
-}
-
-Game::Game(string n, string s) {
-    name = n;
-    story = s;
+    fstream file("story.txt", ios::in);
+    stringstream buffer;
+    buffer << file.rdbuf();
+    file.close();
+    
+    name = "Shawarma Land";
+    story = buffer.str();
 }
 
 string Game::getName() {
@@ -24,35 +24,6 @@ bool Game::getCleared() {
     return cleared;
 }
 
-void Game::setName(string n) {
-    this->name = n;
-}
-
-void Game::setStory(string s) {
-    story = s;
-}
-
 void Game::setCleared(bool c) {
     cleared = c;
-}
-
-void Game::customize(bool c) {
-    string input;
-    if (c) {
-        cout << "What would you like to call the game?" << endl;
-        getline(cin, input);
-        name = input;
-        cout << "Please enter the prologue of the game" << endl;
-        getline(cin, input);
-        story = input;
-    } else {
-        fstream file("story.txt", ios::in);
-        string storyline;
-        string line;
-        while(getline(file, line)) {
-            storyline += line + "\n";
-        }
-        name = "Shawarma Land";
-        story = storyline;
-    }
 }
