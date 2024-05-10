7c3966bb57bd8cfe29188b1c71fc537d586a9509
cbgiveup <168111022+cbgiveup@users.noreply.github.com>
2024-05-10 10:24:41 -0400

    Update coa.h

--- coa.h
+++ coa.h
@@ -5,16 +5,13 @@ using namespace std;
 // COA does not stand for "Canadian Orthopaedic Association," but rather "Clumps of Atoms."
 class COA {
 protected:
-    // static const int NUM_OF_STATS = 3;
     string name;
     string emoji;
-    // int stats[NUM_OF_STATS];
     int health;
     int speed;
     int strength;
 public:
     COA();
-    // COA(string, int, int, int);
     virtual string getName();
     virtual int getHealth();
     virtual int getSpeed();
